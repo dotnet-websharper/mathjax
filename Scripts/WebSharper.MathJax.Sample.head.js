04b6cac0f12e5fc8073f6d44f9f0b01b68b1cfb6
Jooseppi12 <Jooseppi12@users.noreply.github.com>
2025-06-17 18:59:18 +0000

    deploy: 0c382368c0f8bf362d900b910d47bf5a1ada299f

---
+++ Scripts/WebSharper.MathJax.Sample.head.js
@@ -0,0 +1,3 @@
+var wsbundle=(()=>{document.write(`<script>\r
+\r
+<\/script><script src="https://cdn.jsdelivr.net/npm/mathjax@3.2.2/es5/tex-mml-chtml.js" type="text/javascript" charset="UTF-8"><\/script>`);})();
